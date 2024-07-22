b72511af2d0173f8522213a43c121224bd06ccd3
Sofiia Olshevska <olshevskaya.sofia@gmail.com>
2024-07-22 12:20:24 +0200

    feat: create system structs

--- Source.c
+++ Source.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+typedef struct student {
+  char name[20];
+  unsigned int id;
+} Student;
+
+typedef struct course {
+  char name[20];
+  double averageGrade;
+  unsigned int totalStudents;
+  Student* studentList;
+} Course;
+
+typedef struct school {
+  char name[20];
+  unsigned int id;
+  Course* courses;
+} School;
+
 int main() {
   return 0;
 }
