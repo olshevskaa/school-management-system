34cf89bb643adc525391aff5e41f564341a5f930
Sofiia Olshevska <olshevskaya.sofia@gmail.com>
2024-07-23 10:14:06 +0200

    feat: add school creation

--- Source.c
+++ Source.c
@@ -16,6 +16,7 @@ typedef struct course {
 typedef struct school {
   char name[20];
   unsigned int id;
+  unsigned int totalCourses;
   Course* courses;
 } School;
 
@@ -59,6 +60,33 @@ Course* createCourse() {
     Student* newStudent = createStudent();
     newCourse->studentList[i] = *newStudent;
     free(newStudent);
+  }
+}
+
+School* createSchool() {
+  School* newSchool = (School*)malloc(sizeof(School));
+  if(newSchool == NULL) {
+    printf("Something went wrong");
+    exit(1);
+  }
+
+  printf("Enter school name: ");
+  scanf("%s", newSchool->name);
+
+  printf("Enter number of courses: ");
+  scanf("%u", newSchool->totalCourses);
+
+  newSchool->courses = (Course*)malloc(sizeof(Course) * newSchool->totalCourses);
+  if(newSchool->courses == NULL) {
+    printf("Something went wrong");
+    exit(1);
+  }
+
+  for(int i = 0; i < newSchool->totalCourses; i++) {
+    printf("Enter details for course #%d\n", i + 1);
+    Course* newCourse = createCourse();
+    newSchool->courses[i] = *newCourse;
+    free(newCourse);
   }
 }
 
